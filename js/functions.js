bf70d961e7ed9c43321c48ff267207c1631aba8b
Mauro Ruiz <mauror@MacBook-Pro-de-Mauro.local>
2021-08-09 12:01:05 -0300

    subida 09-08-21 12:01

--- js/functions.js
+++ js/functions.js
@@ -91,7 +91,11 @@ function closee() {
     document.getElementById("txt").innerHTML= "select a <br> subfloor";
   }
 
+
+
   function floor_1() {
+    document.getElementById("floor").innerHTML= document.getElementById("floor_selected_1").innerHTML;
+    document.getElementById("txt").innerHTML= "Selected";
     document.getElementById("txt").style.display = "flex";
     document.getElementById("floor").style.display = "flex";
     document.getElementById("txt_cancel").style.display = "none"; 
@@ -100,11 +104,130 @@ function closee() {
     document.getElementById("txt_open").style.display = "none"; 
     document.getElementById("txt_alarm").style.display = "none"; 
     document.getElementById("txt_call").style.display = "none"; 
-    document.getElementById("txt_stop").style.display = "none"; 
-    document.getElementById("floor").innerHTML= "1";
-  }
+    document.getElementById("txt_stop").style.display = "none";  
+  };
 
-  function floorNumbers() {
-    let flooornumber = document.getElementsByClassName("btn_number").value;
-    document.getElementById("floor").innerHTML = flooornumber;
-  }
+  function floor_2() {
+    document.getElementById("floor").innerHTML= document.getElementById("floor_selected_2").innerHTML;
+    document.getElementById("txt").innerHTML= "Selected";
+    document.getElementById("txt").style.display = "flex";
+    document.getElementById("floor").style.display = "flex";
+    document.getElementById("txt_cancel").style.display = "none"; 
+    document.getElementById("txt_ok").style.display = "none"; 
+    document.getElementById("txt_close").style.display = "none"; 
+    document.getElementById("txt_open").style.display = "none"; 
+    document.getElementById("txt_alarm").style.display = "none"; 
+    document.getElementById("txt_call").style.display = "none"; 
+    document.getElementById("txt_stop").style.display = "none";  
+  };
+
+  function floor_3() {
+    document.getElementById("floor").innerHTML= document.getElementById("floor_selected_3").innerHTML;
+    document.getElementById("txt").innerHTML= "Selected";
+    document.getElementById("txt").style.display = "flex";
+    document.getElementById("floor").style.display = "flex";
+    document.getElementById("txt_cancel").style.display = "none"; 
+    document.getElementById("txt_ok").style.display = "none"; 
+    document.getElementById("txt_close").style.display = "none"; 
+    document.getElementById("txt_open").style.display = "none"; 
+    document.getElementById("txt_alarm").style.display = "none"; 
+    document.getElementById("txt_call").style.display = "none"; 
+    document.getElementById("txt_stop").style.display = "none";  
+  };
+
+  function floor_4() {
+    document.getElementById("floor").innerHTML= document.getElementById("floor_selected_4").innerHTML;
+    document.getElementById("txt").innerHTML= "Selected";
+    document.getElementById("txt").style.display = "flex";
+    document.getElementById("floor").style.display = "flex";
+    document.getElementById("txt_cancel").style.display = "none"; 
+    document.getElementById("txt_ok").style.display = "none"; 
+    document.getElementById("txt_close").style.display = "none"; 
+    document.getElementById("txt_open").style.display = "none"; 
+    document.getElementById("txt_alarm").style.display = "none"; 
+    document.getElementById("txt_call").style.display = "none"; 
+    document.getElementById("txt_stop").style.display = "none";  
+    };
+
+  function floor_5() {
+    document.getElementById("floor").innerHTML= document.getElementById("floor_selected_5").innerHTML;
+    document.getElementById("txt").innerHTML= "Selected";
+    document.getElementById("txt").style.display = "flex";
+    document.getElementById("floor").style.display = "flex";
+    document.getElementById("txt_cancel").style.display = "none"; 
+    document.getElementById("txt_ok").style.display = "none"; 
+    document.getElementById("txt_close").style.display = "none"; 
+    document.getElementById("txt_open").style.display = "none"; 
+    document.getElementById("txt_alarm").style.display = "none"; 
+    document.getElementById("txt_call").style.display = "none"; 
+    document.getElementById("txt_stop").style.display = "none";  
+    };
+
+  function floor_6() {
+    document.getElementById("floor").innerHTML= document.getElementById("floor_selected_6").innerHTML;
+    document.getElementById("txt").innerHTML= "Selected";
+    document.getElementById("txt").style.display = "flex";
+    document.getElementById("floor").style.display = "flex";
+    document.getElementById("txt_cancel").style.display = "none"; 
+    document.getElementById("txt_ok").style.display = "none"; 
+    document.getElementById("txt_close").style.display = "none"; 
+    document.getElementById("txt_open").style.display = "none"; 
+    document.getElementById("txt_alarm").style.display = "none"; 
+    document.getElementById("txt_call").style.display = "none"; 
+    document.getElementById("txt_stop").style.display = "none";    
+  };
+
+  function floor_7() {
+    document.getElementById("floor").innerHTML= document.getElementById("floor_selected_7").innerHTML;
+    document.getElementById("txt").innerHTML= "Selected";
+    document.getElementById("txt").style.display = "flex";
+    document.getElementById("floor").style.display = "flex";
+    document.getElementById("txt_cancel").style.display = "none"; 
+    document.getElementById("txt_ok").style.display = "none"; 
+    document.getElementById("txt_close").style.display = "none"; 
+    document.getElementById("txt_open").style.display = "none"; 
+    document.getElementById("txt_alarm").style.display = "none"; 
+    document.getElementById("txt_call").style.display = "none"; 
+    document.getElementById("txt_stop").style.display = "none";    
+  };
+
+  function floor_8() {
+   document.getElementById("floor").innerHTML= document.getElementById("floor_selected_8").innerHTML;
+    document.getElementById("txt").innerHTML= "Selected";
+    document.getElementById("txt").style.display = "flex";
+    document.getElementById("floor").style.display = "flex";
+    document.getElementById("txt_cancel").style.display = "none"; 
+    document.getElementById("txt_ok").style.display = "none"; 
+    document.getElementById("txt_close").style.display = "none"; 
+    document.getElementById("txt_open").style.display = "none"; 
+    document.getElementById("txt_alarm").style.display = "none"; 
+    document.getElementById("txt_call").style.display = "none"; 
+    document.getElementById("txt_stop").style.display = "none";   
+};
+
+function floor_9() {
+  document.getElementById("floor").innerHTML= document.getElementById("floor_selected_9").innerHTML;
+  document.getElementById("txt").innerHTML= "Selected";
+  document.getElementById("txt").style.display = "flex";
+  document.getElementById("floor").style.display = "flex";
+  document.getElementById("txt_cancel").style.display = "none"; 
+  document.getElementById("txt_ok").style.display = "none"; 
+  document.getElementById("txt_close").style.display = "none"; 
+  document.getElementById("txt_open").style.display = "none"; 
+  document.getElementById("txt_alarm").style.display = "none"; 
+  document.getElementById("txt_call").style.display = "none"; 
+  document.getElementById("txt_stop").style.display = "none";    
+};
+function floor_0() {
+  document.getElementById("floor").innerHTML= document.getElementById("floor_selected_0").innerHTML;
+  document.getElementById("txt").innerHTML= "Selected";
+  document.getElementById("txt").style.display = "flex";
+  document.getElementById("floor").style.display = "flex";
+  document.getElementById("txt_cancel").style.display = "none"; 
+  document.getElementById("txt_ok").style.display = "none"; 
+  document.getElementById("txt_close").style.display = "none"; 
+  document.getElementById("txt_open").style.display = "none"; 
+  document.getElementById("txt_alarm").style.display = "none"; 
+  document.getElementById("txt_call").style.display = "none"; 
+  document.getElementById("txt_stop").style.display = "none";    
+};
